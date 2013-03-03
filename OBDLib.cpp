2b3c497e8586138f312a8703dcf78701b276181c
gregpaton08 <gregpaton08@gmail.com>
2013-03-03 17:24:17 -0500

    [add] First commit.

---
+++ OBDLib.cpp
@@ -0,0 +1,16 @@
+#include "OBDLib.h"
+
+/*! \brief Class constructor
+ *
+ */
+OBDLib::OBDLib() {
+    
+}
+
+/*! \brief Method to initialize connection to OBD
+ *
+ * \return True if connected. False otherwise.
+ */
+bool OBDLib::init() {
+    
+}
---
+++ OBDLib.h
@@ -0,0 +1,15 @@
+//
+// OBDLib.h
+// OBDLib
+//
+// Created by Greg Paton on 3/3/2013
+//
+//
+
+class OBDLib {
+public:
+    OBDLib();
+    bool init();
+private:
+    
+};
